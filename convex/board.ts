e709f7bdd462c0bc8815f54ee13eb8e8c3912c2a
dnguye12 <duchuyng051@gmail.com>
2025-01-03 08:36:04 +0100

    boards searching

--- convex/board.ts
+++ convex/board.ts
@@ -48,6 +48,20 @@ export const remove = mutation({
             throw new Error("Unauthorized")
         }
 
+        const userId = identity.subject
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board", (q) =>
+                q.eq("userId", userId)
+                    .eq("boardId", args.id)
+            )
+            .unique()
+
+        if (existingFavorite) {
+            await ctx.db.delete(existingFavorite._id)
+        }
+
         await ctx.db.delete(args.id)
     }
 })
@@ -98,10 +112,9 @@ export const favorite = mutation({
 
         const userId = identity.subject
 
-        const existingFavorite = await ctx.db.query("userFavorites").withIndex("by_user_board_org", (q) =>
+        const existingFavorite = await ctx.db.query("userFavorites").withIndex("by_user_board", (q) =>
             q.eq("userId", userId)
                 .eq("boardId", board._id)
-                .eq("orgId", args.orgId)
         ).unique()
 
         if (existingFavorite) {
